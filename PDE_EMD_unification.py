0beaa6c86c632eba75ddde66f7a0da9db4321198
Fyrst-Walther <61984625+Fyrst-Walther@users.noreply.github.com>
2022-06-24 12:51:28 +0200

    Update PDE_EMD_unification.py

--- PDE_EMD_unification.py
+++ PDE_EMD_unification.py
@@ -14,11 +14,11 @@ as described in the report
 import numpy as np
 import Modules.PDE_EMD as PDE
 
-q = 288
+q = 576
 T = 6
-s = 4
+s = 5
 boundary = "Neumann_0"
-data_type = "train"
+data_type = "test"
 if data_type == "train":
     decomposition = np.load(f"Data/PDE_Window_q{q}_T{T}_{boundary}.npy")
 else:
@@ -26,41 +26,7 @@ else:
 n, m, q = np.shape(decomposition)
 train_mesh = np.load(f"Data/train_mesh_q{q}.npy")
 test_mesh = np.load(f"Data/test_mesh_q{q}.npy")
-
-
-# Unification in IMFs
-T_line = np.arange(0, n)
-unified_4 = np.zeros((n,4,q)).astype(np.float32)
-if data_type == "train":
-    try:
-        unified_4 = np.load(f"Data/PDE_Window_IMFs_q{q}_T{T}_{boundary}.npy")
-    except  FileNotFoundError:
-        for i in range(n):
-            unified_4[i,3,:] = np.sum(decomposition[i,4:,:], axis = 0)
-            for j in range(4):
-                max_pos, min_pos, _ = PDE.PDE_EMD.find_extrema(T_line, decomposition[i,j,:])
-                if len(max_pos) + len(min_pos) < 4:
-                    unified_4[i,3,:] += decomposition[i,j,:]
-                elif j == 2 or j == 3:
-                    unified_4[i,2,:] += decomposition[i,j,:]
-                else:
-                    unified_4[i,j,:] += decomposition[i,j,:]
-        np.save(f"Data/PDE_Window_IMFs_q{q}_T{T}_{boundary}.npy", unified_4)
-else:
-    try:
-        unified_4 = np.load(f"Data/PDE_Window_IMFs_Test_q{q}_T{T}_{boundary}.npy")
-    except  FileNotFoundError:
-        for i in range(n):
-            unified_4[i,3,:] = np.sum(decomposition[i,4:,:], axis = 0)
-            for j in range(s):
-                max_pos, min_pos, _ = PDE.PDE_EMD.find_extrema(T_line, decomposition[i,j,:])
-                if len(max_pos) + len(min_pos) < 4:
-                    unified_4[i,3,:] += decomposition[i,j,:]
-                elif j == 2 or j == 3:
-                    unified_4[i,2,:] += decomposition[i,j,:]
-                else:
-                    unified_4[i,j,:] += decomposition[i,j,:]
-        np.save(f"Data/PDE_Window_IMFs_Test_q{q}_T{T}_{boundary}.npy", unified_4)    
+   
 
 # Unification in residual
 T_line = np.arange(0, n)
